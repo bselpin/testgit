c620a0dfc1756098f37903826c469901726a932a
bselpin <bselpin@naver.com>
2021-09-22 15:30:29 +0900

    no message

--- src/components/case/AnswerCard.js
+++ src/components/case/AnswerCard.js
@@ -15,7 +15,7 @@ const AnswerCard = ({ answer }) => {
 		pttnCd,
 	} = answer
 
-	console.log(answer)
+	console.log(answer, "test")
 
 	const toDetailUrl = category => {
 		if (category && category !== "undefined" && category !== "13") {
